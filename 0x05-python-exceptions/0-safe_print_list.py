86d8c043d74d3f27155e34087fdc5640cdcc793a
wilfex81 <84175893+wilfex81@users.noreply.github.com>
2022-06-20 17:55:18 +0300

    Fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-
-    for i in range(x):
+    counter = 0
+    for a in range(0, x):
         try:
-            print("{}".format(my_list[i]), end='')
-        except:
+            print(my_list[a], end="")
+            counter = counter + 1
+        except IndexError:
             break
-        else:
-            count += 1
-
     print()
-    return (count)
+    return counter
