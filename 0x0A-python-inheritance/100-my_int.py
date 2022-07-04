a0d59a7c3fa87ac0f83544d21a3d1d2452bb06ee
wilfex81 <84175893+wilfex81@users.noreply.github.com>
2022-07-04 21:50:40 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""initializate"""
+
+
 class MyInt(int):
-    """ Class that inherits from class int"""
+    """class MyInt"""
 
-    def __eq__(self, other):
-        """ Method that returns != check """
-        return int.__ne__(self, other)
+    def __ne__(self, next):
+        """negation to equal"""
+        return super().__eq__(next)
 
-    def __ne__(self, other):
-        """ Method that returns == check """
-        return int.__eq__(self, other)
+    def __eq__(self, next):
+        """equal to negation"""
+        return super().__ne__(next)
