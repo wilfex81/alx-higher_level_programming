d1383ea896e12cf8fa18191dd5261e9b0a292baf
wilfex81 <84175893+wilfex81@users.noreply.github.com>
2022-07-05 23:17:17 +0300

    Files:8

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# 101-stats.py
-# Brennan D Baraban <375@holbertonschool.com>
+
+
+
 """Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
@@ -26,7 +27,6 @@ if __name__ == "__main__":
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
-
     try:
         for line in sys.stdin:
             if count == 10:
@@ -36,12 +36,10 @@ if __name__ == "__main__":
                 count += 1
 
             line = line.split()
-
             try:
                 size += int(line[-1])
             except (IndexError, ValueError):
                 pass
-
             try:
                 if line[-2] in valid_codes:
                     if status_codes.get(line[-2], -1) == -1:
@@ -50,9 +48,7 @@ if __name__ == "__main__":
                         status_codes[line[-2]] += 1
             except IndexError:
                 pass
-
         print_stats(size, status_codes)
-
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
