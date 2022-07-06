7df3a7ca2283f09440d7107ac5b6baa7c8111e08
wilfex81 <84175893+wilfex81@users.noreply.github.com>
2022-07-06 11:08:26 +0300

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,15 +8,12 @@ prints the following statistics:
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
+    """Print accumulated metrics."""
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
         
+
 
 if __name__ == "__main__":
     import sys
