2c92e497b954c1c13e33b635c60715b868128a78
wilfex <wilfex81@gmail.com>
2022-09-23 21:34:05 +0300

    files

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""
-Python script that fetches an URL with requests package
-"""
+"""initializate"""
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+def status():
+    """a Python script that fetches https://intranet.hbtn.io/status"""
+
+    data_req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}".
+          format(type(data_req.text), data_req.text))
+
+
+if __name__ == '__main__':
+    status()
