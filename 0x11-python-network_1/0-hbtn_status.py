ca02d8a69b47469d8aed66bed38d741d35ad89e3
wilfex <wilfex81@gmail.com>
2022-09-23 21:21:04 +0300

    files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
+"""initializate"""
+from urllib.request import urlopen
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+def hbtn_status_0():
+    """function show my status"""
+    with urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+        utf8 = html.decode('utf-8')
+        print("Body response:\n\t- type: {}".format(type(html)))
+        print("\t- content: {}\n\t- utf8 content: {}".
+              format(html, utf8, end=""))
+
+
+if __name__ == '__main__':
+    hbtn_status_0()
