d1228635dff64ff7b9b77bd27d4dbf469bf4d112
wilfex81 <84175893+wilfex81@users.noreply.github.com>
2022-10-18 16:32:52 +0300

    files

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
+const epId = process.argv[2];
+const apiUrl = `https://swapi.co/api/films/${epId}`;
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const charList = JSON.parse(body).characters;
+    for (let i = 0; i < charList.length; i++) {
+      request.get(charList[i], (err, res, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const names = JSON.parse(body).name;
+          console.log(names);
         }
       });
     }
